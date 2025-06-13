28febc0075a7a12126e943d866234d032d305b19
Ajoro <folkeeert@gmail.com>
2025-06-13 15:21:35 +0200

    init

---
+++ PluginConfiguration.cs
@@ -0,0 +1,10 @@
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Controller.Plugins;
+
+namespace JellyfinRemote
+{
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        // Konfigurationsfält kan läggas till här senare
+    }
+}
